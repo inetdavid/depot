397fde0f2a90b6dd704bb52840f833630cef901d
David Schmidt <dschmidt@ciena.com>
2012-04-18 16:41:11 -0700

    Fresh rails 3.2.3 project with rvmrc and depot gemset

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'd901e283247fca84eac57148068167b34bfd25aa7b3d602b7458cf7c4188566c714ac42edb563d15257b1ac9364e3118e4936d26e87561889280cc96189fa561'
