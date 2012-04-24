a9b33f30262957f843c8902e1f3bbc7379f235cb
David Schmidt <davids@tower-mt.com>
2012-04-23 21:24:37 -0700

    10.2 Combine duplicate line_items in database and handle bad cart ids using Flash

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120424013759) do
+ActiveRecord::Schema.define(:version => 20120424030900) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
