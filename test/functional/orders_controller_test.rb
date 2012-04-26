30a321adde4ab2f288dcf40e7af44f34f307fb60
David Schmidt <davids@tower-mt.com>
2012-04-25 20:02:59 -0700

    12.1 Fixed tests and my mistake in cart partial

--- test/functional/orders_controller_test.rb
+++ test/functional/orders_controller_test.rb
@@ -20,7 +20,7 @@ class OrdersControllerTest < ActionController::TestCase
   test "should get new" do
     cart = Cart.create
     session[:cart_id] = cart.id
-    LineItem.create(cart: cart, product: products(:ruby))
+    LineItem.create(cart_id: cart.id, product_id: products(:ruby).id)
     
     get :new
     assert_response :success
@@ -28,7 +28,7 @@ class OrdersControllerTest < ActionController::TestCase
 
   test "should create order" do
     assert_difference('Order.count') do
-      post :create, order: @order.attributes
+      post :create, order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type }
     end
 
     assert_redirected_to store_path
